df1f2dff27fbc4b7ec809e368ef2669b44b86fd4
Grazielle Nascimento <grazigrazielle454@gmail.com>
2024-11-23 14:20:28 -0300

    feat: implement server setup with Express and dotenv; configure database connection

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const { sequelize } = require('./server');
+const hotelRoutes = require('./routes/hotelRoutes');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+// app.use('/api/hotels', hotelRoutes);
+
+/* sequelize
+  .authenticate()
+  .then(() => {
+    console.log(
+      'Connection to the database has been established successfully.'
+    );
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+sequelize.sync({ force: false }).then(() => {
+  console.log('Database synchronized');
+}); */
+
+const PORT = process.env.DB_PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
