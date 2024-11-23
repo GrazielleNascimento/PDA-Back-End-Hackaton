9eba0e3d660cfc56fc8ac7281adbce4b5fe23700
Grazielle Nascimento <grazigrazielle454@gmail.com>
2024-11-23 15:46:59 -0300

    feat: add error handling middleware to server.js

--- server.js
+++ server.js
@@ -30,3 +30,6 @@ const PORT = process.env.DB_PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+const errorHandler = require('./middlewares/errorHandler');
+app.use(errorHandler);
