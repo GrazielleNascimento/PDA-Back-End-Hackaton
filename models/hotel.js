4d2112680349703a90884e91caba883a4f93a0d1
Grazielle Nascimento <grazigrazielle454@gmail.com>
2024-11-23 21:55:42 -0300

    feat: add reviews and timestamps to hotel model

--- models/hotel.js
+++ models/hotel.js
@@ -11,78 +11,78 @@ const Hotel = sequelize.define(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: { 
-      type: DataTypes.STRING, 
-      allowNull: false 
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    stars: { 
-      type: DataTypes.INTEGER, 
-      allowNull: false 
+    stars: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
-    latitude: { 
-      type: DataTypes.STRING, 
-      allowNull: false 
+    latitude: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    longitude: { 
-      type: DataTypes.STRING, 
-      allowNull: false 
+    longitude: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    description: { 
-      type: DataTypes.TEXT, 
-      allowNull: true 
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
-    address: { 
-      type: DataTypes.STRING, 
-      allowNull: false 
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    district: { 
-      type: DataTypes.STRING, 
-      allowNull: true 
+    district: {
+      type: DataTypes.STRING,
+      allowNull: true,
     },
-    city: { 
-      type: DataTypes.STRING, 
-      allowNull: false 
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    state: { 
-      type: DataTypes.STRING, 
-      allowNull: false 
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    country: { 
-      type: DataTypes.STRING, 
-      allowNull: false 
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    placeid: { 
-      type: DataTypes.STRING, 
-      allowNull: false 
+    placeid: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    thumb: { 
-      type: DataTypes.STRING, 
-      allowNull: true 
+    thumb: {
+      type: DataTypes.STRING,
+      allowNull: true,
     },
-    images: { 
-      type: DataTypes.JSON, 
-      allowNull: true 
+    images: {
+      type: DataTypes.JSON,
+      allowNull: true,
     },
-    amenities: { 
+    amenities: {
       type: DataTypes.JSON,
     },
-    pois: { 
-      type: DataTypes.JSON, 
-      allowNull: true 
+    pois: {
+      type: DataTypes.JSON,
+      allowNull: true,
     },
-    reviews: { 
-      type: DataTypes.JSON, 
-      allowNull: true 
+    reviews: {
+      type: DataTypes.JSON,
+      allowNull: true,
     },
-     createdat: { 
-      type: DataTypes.DATE, 
-      allowNull: false, 
-      defaultValue: DataTypes.NOW 
+    createdat: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
     },
-    updatedat: { 
-      type: DataTypes.DATE, 
-      allowNull: true 
-    }, 
+    updatedat: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
   },
   {
     tableName: 'hotels',
