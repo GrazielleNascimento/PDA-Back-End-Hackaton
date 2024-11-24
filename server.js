c181a6c24777e439d5c0ac5d3320b403c824a305
Grazielle Nascimento <grazigrazielle454@gmail.com>
2024-11-24 14:20:25 -0300

    feat: enable CORS for API to allow requests from localhost

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require('express');
 const dotenv = require('dotenv');
 const { sequelize } = require('./config');
 const hotelRoutes = require('./routes/hotelRoutes');
-
+const cors = require ('cors');
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+ app.use(cors({
+    origin: 'http://localhost:5500',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+})) 
+
 
 app.use('/api/hotels', hotelRoutes);
 
